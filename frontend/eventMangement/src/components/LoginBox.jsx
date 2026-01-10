011d393e8e1c48ad3545aac9be97e7a5464d3d96
Nikita-Darji <darjinikita2001@gmail.com>
2026-01-10 22:36:02 +0530

    Fetched SignIn and LogIn Data

---
+++ frontend/eventMangement/src/components/LoginBox.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+export default function LoginBox({onClose}) {
+    
+    const [email,setemail]=useState('')
+    const [password,setpassword]=useState('')
+    const [isSignUp,setisSignUp]=useState(false)
+
+    // localStorage.clear()
+
+    const handleSubmit=async(e)=>{
+        e.preventDefault()
+        let SignUp = isSignUp?'login':'signUp'
+        await axios.post(`http://localhost:5000/${SignUp}`,{email,password})
+        .then((res)=>{
+            localStorage.setItem('token',res.data.token)
+            localStorage.setItem('user',JSON.stringify(res.data.user))
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+    }
+
+  return (
+
+ <div className="fixed top-10 w-full min-h-screen flex items-center justify-center bg-gray-100 z-2">
+
+  <div className="bg-white p-8 rounded-lg shadow-md w-96">
+     <button onClick={onClose} className=" text-gray-700 hover:text-black text-2xl z-2"
+        >
+          ×
+        </button>
+    <h2 className="text-2xl font-semibold text-center mb-6">{isSignUp?'LogIn':'SignIn'}</h2>
+
+    <form className="space-y-2" onSubmit={handleSubmit}>
+      <input
+        onChange={(e)=>setemail(e.target.value)}
+        type="email"
+        placeholder="Email"
+        className="w-full border px-4 py-2 rounded"
+      />
+
+      <input
+        onChange={(e)=>setpassword(e.target.value)}
+        type="password"
+        placeholder="Password"
+        className="w-full border px-4 py-2 rounded"
+      />
+
+      <button
+        type="submit"
+        className="w-full bg-teal-600 text-white py-2 rounded hover:bg-teal-700"
+      >
+        {isSignUp?'LogIn':'SignIn'}
+      </button>
+    </form>
+
+    <p className="text-sm text-center mt-4" onClick={()=>setisSignUp(pre=>!pre)}>
+     {isSignUp?'Don’t have an account?':'Already have account'}
+    </p>
+  </div>
+
+</div>
+
+  )
+}
--- frontend/eventMangement/src/components/Navbar.jsx
+++ frontend/eventMangement/src/components/Navbar.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import LoginBox from './LoginBox'
 
 export default function Navbar() {
     // useEffect(asyn(req,res) => {
     //   const user = localStorage.get()
     // }, [])
-    
+    const [isOpen,setisOpen]=useState(false)
+
+    const checkOpen=()=>{
+        setisOpen(true)
+    }
     return (
         <>
-            <nav className="bg-teal-900 text-white px-6 py-4 flex justify-between items-center">
+            <nav className="bg-teal-900 text-white px-6 py-4 flex justify-between items-center fixed w-full z-3">
 
                 {/* Left side */}
                 <div className="text-xl font-semibold">
@@ -25,12 +30,14 @@ export default function Navbar() {
                     <li className="hover:text-gray-300 cursor-pointer">
                         My Events
                     </li>
-                    <Link to="/login"><li className="hover:text-gray-300 cursor-pointer">
+                    <li onClick={checkOpen} className="hover:text-gray-300 cursor-pointer">
                         LogIn
-                    </li></Link>
+                    </li>
                 </ul>
 
             </nav>
+
+            {(isOpen) && <LoginBox onClose={()=>setisOpen(false)}/>}
 
         </>
     )
